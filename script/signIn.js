c41013bc4bc34f36c045e900e8a3131265251602
Harsha-C2508 <99667782+Harsha-C2508@users.noreply.github.com>
2022-02-24 23:39:29 +0530

    signIn_timer_Cw2

---
+++ script/signIn.js
@@ -0,0 +1,19 @@
+document.querySelector("#form").addEventListener("submit", myLogin);
+
+var signupArr = JSON.parse(localStorage.getItem("signUpDetails")) || [];
+function myLogin(event){
+    event.preventDefault();
+    var signupObj = {
+       email : document.querySelector("#email").value,
+    };
+
+    signupArr.push(signupObj)
+    console.log(signupArr)
+    localStorage.setItem("signUpDetails", JSON.stringify(signupArr));
+
+    document
+       .querySelector("input[type='submit']")
+       .addEventListener("click",function(){
+        window.location.href="./timer.html"
+    });            
+}
