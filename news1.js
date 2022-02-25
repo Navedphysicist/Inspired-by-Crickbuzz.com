4363c13f5308965180d68c2c5ed66ec412d1a51d
Rahul gour <96679977+rahulgourvlog@users.noreply.github.com>
2022-02-25 16:11:04 +0530

    my cw project
    submit

---
+++ news1.js
@@ -0,0 +1,21 @@
+var arrayofobj=[{img:"https://www.cricbuzz.com/a/img/v1/100x77/i1/c226918/india-vs-west-indies-3rd-t20i.jpg"
+,head:"India vs West Indies, 3rd T20I",
+time:"Sun, Feb 20 2022"},
+{img:"https://www.cricbuzz.com/a/img/v1/100x77/i1/c226867/australia-vs-sri-lanka-5th-t2.jpg"
+,head:"Australia vs Sri Lanka, 5th T20",
+time:"Sun, Feb 20 2022"},
+{img:"https://www.cricbuzz.com/a/img/v1/100x77/i1/c226805/india-vs-west-indies-2nd-t20i.jpg",
+head:"India vs West Indies, 2nd T20I",
+time:"Fri, Feb 18 2022"},
+{img:"https://www.cricbuzz.com/a/img/v1/100x77/i1/c226748/australia-vs-sri-lanka-4th-t2.jpg",
+head:"Australia vs Sri Lanka, 4th T20",
+time:"Fri, Feb 18 2022"},
+{img:"https://www.cricbuzz.com/a/img/v1/100x77/i1/c226688/new-zealand-vs-south-africa-1.jpg",
+head:"New Zealand vs South Africa, 1st Test, Day 1",
+time:"Thu, Feb 17 2022"},
+{img:"https://www.cricbuzz.com/a/img/v1/100x77/i1/c226200/australia-vs-sri-lanka-1st-t2.jpg",
+head:"Australia vs Sri Lanka, 1st",
+time:"Fri, Feb 11 2022"},]
+
+
+localStorage.setItem("latest",JSON.stringify(arrayofobj));
